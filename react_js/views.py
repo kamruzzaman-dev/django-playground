e929ed26bce27168f3e1b241d2360bca6eeaa729
kamruzzaman-dev <kamruzzamanmd@hotmail.com>
2025-04-29 19:59:41 +0600

    Filters and Tags on the Template Language

--- react_js/views.py
+++ react_js/views.py
@@ -2,7 +2,17 @@ from django.shortcuts import HttpResponse, render
 
 # Create your views here.
 def react_js(request):
-    return render(request, "React/react.html")
+    course ='React JS and Next.js'
+    total_class= 30
+    seat= 25
+    class_duration= '3 months'
+    offering = {
+        # 'course': course,
+        'total_class': total_class,
+        'seat': seat,
+        'class_duration': class_duration
+    }
+    return render(request, "React/react.html", context=offering)
 
 def home(request):
     name = {'which':"React"}
